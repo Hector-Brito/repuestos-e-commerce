3d11bb234f7ad604f894f239d7626972d9aae921
Tcraken <tcraken4@gmail.com>
2025-04-06 15:46:22 -0400

    Agredado reporte template de reportes diarios

--- src/reportes/reportes.controller.ts
+++ src/reportes/reportes.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { ReportesService } from './reportes.service';
+import { Public } from 'src/auth/decorators/isPublic.decorator';
+import { Response } from 'express';
 
 @Controller('reportes')
 export class ReportesController {
   constructor(private readonly reportesService: ReportesService) {}
+
+
+  @Get('hoy')
+  @Public()
+  async getDailyReport(@Res() res:Response){
+    const pdf = await this.reportesService.generateDailyReport()
+    res.setHeader('Content-Type', 'application/pdf')
+    res.setHeader('Content-Disposition', 'inline; filename=sales-report.pdf')
+    res.end(pdf)
+  }
 }
--- src/reportes/reportes.service.ts
+++ src/reportes/reportes.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
-
+import * as fs from 'fs';
+import * as path from 'path';
+import * as handlebars from 'handlebars';
+import puppeteer from 'puppeteer';
 
 
 @Injectable()
@@ -7,12 +10,34 @@ export class ReportesService {
 
     constructor(){}
 
-
-    async generateDailyReport(){
-
+    async getTemplate(name:string){
+        const templateHtml = fs.readFileSync(path.join(process.cwd(),'src','reportes','reportes-templates',`${name}.html`),'utf-8')
+        const template = handlebars.compile(templateHtml)
+        return template
     }
 
-    async generateMonthlyReport(){}
+    async generateDailyReport(){
+        const data = {
+            titulo:'REPORTE DE VENTAS',
+            empresa:'AUTOPARTES-MATURIN'
+        }
+        const template = await this.getTemplate('daily-report')
+        const html = template(data)
+        const browser = await puppeteer.launch({headless:true,args:['--no-sandbox']})
+        const page = await browser.newPage()
+        await page.setContent(html)
+        const buffer = await page.pdf(
+          {
+            printBackground:true,
+            format:'A4'
+          }
+        )
+        await browser.close()
+        return buffer
+    }
+
+    async generateMonthlyReport(){
+    }
 
     async generateYearlyReport(){}
 
