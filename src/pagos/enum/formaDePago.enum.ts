ccbc86344d7cfbe50a436edde04ad44d0fd2002a
Tcraken <tcraken4@gmail.com>
2025-05-13 19:16:05 -0400

    Añadido forma de pago pagomovil, transferencia y zelle

--- src/pagos/enum/formaDePago.enum.ts
+++ src/pagos/enum/formaDePago.enum.ts
@@ -1,6 +1,9 @@
 
 export enum FormaDePago{
-    PagoMovilOTransferencia="PAGOMOVIL O TRANSFERENCIA",
+    PagoMovilOTransferencia="PAGOMOVIL O TRANSFERENCIA",//HAY OBJETOS CON ESTA PROPIEDAD EN LA BASE DE DATOS, AL QUITARLO ES NECESARIO ELIMINARLOS.
     Efectivo="EFECTIVO",
+    PagoMovil="PAGOMOVIL",
+    Transferencia="TRANSFERENCIA",
+    Zelle="ZELLE",
     
 }
