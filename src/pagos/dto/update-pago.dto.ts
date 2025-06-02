64ff663c623a03e482492b0683dbd3b94a678b6c
Tcraken <hectorbritoalcala@gmail.com>
2025-06-02 08:23:24 -0400

    Agredado tasaBsDelDia a update-pago-dto

--- src/pagos/dto/update-pago.dto.ts
+++ src/pagos/dto/update-pago.dto.ts
@@ -1,5 +1,13 @@
 import { PartialType } from "@nestjs/swagger";
 import { CreatePagoDto } from "../../pagos/dto/create-pago.dto";
+import { IsNotEmpty, IsNumber } from "class-validator";
+import { Type } from "class-transformer";
 
 
-export class UpdatePagoDto extends PartialType(CreatePagoDto){}
+export class UpdatePagoDto extends PartialType(CreatePagoDto){
+    
+    @IsNumber({maxDecimalPlaces:2})
+    @IsNotEmpty()
+    @Type(() => Number)
+    tasaBsDelDia:number
+}
