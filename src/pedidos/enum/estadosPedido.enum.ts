47744e92e80dd08cea6d9d463a484bec197349dc
Tcraken <hectorbritoalcala@gmail.com>
2025-09-15 09:50:33 -0400

    asd

--- src/pedidos/enum/estadosPedido.enum.ts
+++ src/pedidos/enum/estadosPedido.enum.ts
@@ -2,5 +2,6 @@ export enum EstadoDePedido{
     Verificacion='Pedido en verificacion de pago',
     Empaquetado='Pedido en proceso de empaquetado',
     Enviado='Pedido enviado',
-    Cancelado='Cancelado'
+    Cancelado='Cancelado',
+    Disponible='Disponible para entregar',
 }
